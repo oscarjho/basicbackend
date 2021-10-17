785542dad92c0be08d1ea850df016e195cb7efea
oscar <oscarjavierho@gmail.com>
2021-10-16 19:31:01 -0430

    test

--- env.js
+++ env.js
@@ -1,4 +1,4 @@
-const port = 4500;
+const port = process.env.PORT || 80;
 const environment = "develop";
 
 module.exports = {port, environment}
--- server.js
+++ server.js
@@ -9,3 +9,15 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  const name = process.env.NAME || 'World';
+  res.send(`Hello ${name}!`);
+});
+
+app.listen(port, () => {
+  console.log(`helloworld: listening on port ${port}`);
+});
